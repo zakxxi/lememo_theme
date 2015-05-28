70924fdb2527b623c3a5bb9899fac9aaca17c427
Adrien Revel <adrien@abrege.net>
2015-05-28 16:04:53 +0200

    Nice scroll ;-)

--- themes/custom/lememo_theme/scripts.js
+++ themes/custom/lememo_theme/scripts.js
@@ -13,9 +13,14 @@
 		// Sticky contact form
     $(".field-name-contact-form").sticky({topSpacing:100});
 
-
+    // Smooth scroll
+      $('.scrollto').click( function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 900; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        return false;
+      });
 
      // end our js code
-     
    }
  };})(jQuery);
